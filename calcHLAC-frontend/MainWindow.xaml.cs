69969586074a0be6ef75edb48d1dc92167db9847
yanoshi <googreimu@gmail.com>
2015-06-08 17:53:43 +0900

    1.5歩目

--- calcHLAC-frontend/MainWindow.xaml.cs
+++ calcHLAC-frontend/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace calcHLAC_frontend
+namespace Yanoshi.CalcHLACGUI
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
