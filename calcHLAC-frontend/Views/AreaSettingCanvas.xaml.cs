350317a8e3aaee0b4491f9a7afe840a147eb796d
yanoshi <googreimu@gmail.com>
2015-06-10 22:15:28 +0900

    描けるようになりました なお、データの管理はしていない模様

--- calcHLAC-frontend/Views/AreaSettingCanvas.xaml.cs
+++ calcHLAC-frontend/Views/AreaSettingCanvas.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Yanoshi.CalcHLACGUI.ViewModels;
+
 namespace Yanoshi.CalcHLACGUI.Views
 {
     /// <summary>
@@ -25,39 +27,108 @@ namespace Yanoshi.CalcHLACGUI.Views
             InitializeComponent();
         }
 
-
+        private List<Rectangle> rectList = new List<Rectangle>();
 
 
         #region イベント
-        private bool _inDrag = false;
-        private double _diffX;
-        private double _diffY;
+        private bool inDrag = false;
+        private double diffX;
+        private double diffY;
         private void rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            _inDrag = true;
+            inDrag = true;
             Point point = e.GetPosition(mainCanves);
-            _diffX = point.X - Canvas.GetLeft((Rectangle)sender);
-            _diffY = point.Y - Canvas.GetTop((Rectangle)sender);
+            diffX = point.X - Canvas.GetLeft((Rectangle)sender);
+            diffY = point.Y - Canvas.GetTop((Rectangle)sender);
             ((Rectangle)sender).CaptureMouse();
         }
         private void rectangle_MouseMove(object sender, MouseEventArgs e)
         {
-            if (_inDrag)
+            if (inDrag)
             {
                 Point pos = e.GetPosition(mainCanves);
-                Canvas.SetLeft((Rectangle)sender, pos.X - _diffX);
-                Canvas.SetTop((Rectangle)sender, pos.Y - _diffY);
+                Canvas.SetLeft((Rectangle)sender, pos.X - diffX);
+                Canvas.SetTop((Rectangle)sender, pos.Y - diffY);
             }
         }
         private void rectangle_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            _inDrag = false;
+            inDrag = false;
             ((Rectangle)sender).ReleaseMouseCapture();
 
-            _diffX = 0;
-            _diffY = 0;
+            diffX = 0;
+            diffY = 0;
         }
 
+
+
+        private bool isMouseDown = false;
+        private Rectangle nowActiveObj;
+        private double startX = 0, startY = 0;
+        private void grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if(isMouseDown)
+            {
+                Point pos = e.GetPosition(mainCanves);
+                double w = pos.X - startX;
+                double h = pos.Y - startY;
+
+                if (w < 0 || h < 0)
+                {
+                    this.mainCanves.Children.Remove(nowActiveObj);
+                    nowActiveObj.ReleaseMouseCapture();
+                }
+
+                isMouseDown = false;
+            } 
+        }
+
+        private void grid_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                Point pos = e.GetPosition(mainCanves);
+
+                double w = pos.X-startX;
+                double h = pos.Y-startY;
+
+                if (w < 0 || h < 0)
+                    return;
+
+                nowActiveObj.Width=w;
+                nowActiveObj.Height=h;
+            }
+        }
+
+        private void grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (inDrag == false && ((AreaSettingCanvesViewModel)this.DataContext).GivenPictureData != null)
+            {
+                Point pos = e.GetPosition(mainCanves);
+
+                Rectangle rect = new Rectangle();
+                rectList.Add(rect);
+
+                rect.Fill=Brushes.Green;
+                
+                
+                Canvas.SetLeft(rect, pos.X);
+                Canvas.SetTop(rect, pos.Y);
+
+                this.mainCanves.Children.Add(rect);
+
+                
+                startX = pos.X;
+                startY = pos.Y;
+
+                nowActiveObj = rect;
+
+                isMouseDown = true;
+            }
+        }
+        
         #endregion
+
+       
     }
 }
